9085d9be7b003bfe6a8239ec9548fb4bece84074
adryansf <adryansfreitas@gmail.com>
2020-01-16 20:34:55 -0300

    Fixed bugs in the feature for to search devs

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -9,24 +9,34 @@ module.exports = {
     const { latitude, longitude, techs } = req.query;
 
     let query = {};
-    parseStringAsArray(techs).map(tech => {
-      query = { ...query, $regex: new RegExp(`${tech}`, "i") };
-    });
+    if (techs) {
+      parseStringAsArray(techs).map(tech => {
+        query = { ...query, $regex: new RegExp(`${tech}`, "i") };
+      });
+    } else {
+      query = {
+        $regex: new RegExp("[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]")
+      };
+    }
 
-    const devs = await Dev.find({
-      techs: {
-        $elemMatch: query
-      },
-      location: {
-        $near: {
-          $geometry: {
-            type: "Point",
-            coordinates: [longitude, latitude]
-          },
-          $maxDistance: 10000
+    if (latitude && longitude) {
+      const devs = await Dev.find({
+        techs: {
+          $elemMatch: query
+        },
+        location: {
+          $near: {
+            $geometry: {
+              type: "Point",
+              coordinates: [longitude, latitude]
+            },
+            $maxDistance: 10000
+          }
         }
-      }
-    });
-    return res.json(devs);
+      });
+      return res.json(devs);
+    }
+
+    return res.status(400).json({ error: "Don't have coordinates" });
   }
 };
