67e36392c98e2427dbbe7f5e4f1ecc6bdf9edf04
adryansf <adryansfreitas@gmail.com>
2020-01-17 14:44:48 -0300

    Added websockets

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const Dev = require("../models/Dev");
 const github = require("../services/github");
 const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
   async store(req, res) {
@@ -34,6 +35,16 @@ module.exports = {
       techs: techsArray,
       location
     });
+
+    //Filtrar as conexões que estão no máximo a 10km de distância
+    //e que o novo deve tenha pelo menos uma das tecnologias filtradas
+
+    const sendSocketMessageTo = findConnections(
+      { latitude, longitude },
+      techsArray
+    );
+
+    sendMessage(sendSocketMessageTo, "new-dev", dev);
 
     return res.json(dev);
   },
--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 //Modules
 const express = require("express");
 const routes = require("./routes");
+const http = require("http");
 const cors = require("cors");
+const { setupWebSocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 //Database
 require("./database");
@@ -13,4 +18,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
